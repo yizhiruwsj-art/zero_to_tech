84e4ace0a5f597b2bf1addb61d52aac7c433c221
yizhiru <1439546779@qq.com>
2026-08-31 21:19:32 +0800

    变成一个好看网页，吃了好吃的柚子抹茶蛋糕

--- script.js
+++
@@ -1,6 +0,0 @@
-function changeText() {
-    // document用于访问网页的 DOM，console用于在浏览器的控制台输出信息,window.alert用于弹出提示框
-    console.log('按钮被点击了！');
-    document.getElementById('msg').textContent = '你刚刚触发了一段 JavaScript。';
-    window.alert("你好");
-}
